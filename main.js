6cf8fb78770109e817f897ed57a2b8784838b57a
Djawad-boutemine <138432818+Djawad-boutemine@users.noreply.github.com>
2023-07-28 14:25:21 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -6,6 +6,9 @@ let states = [];
 for (let i = 0; i < btns.length; i++) { states.push(false) };
 console.log(states);
 let add = 0 ;
+console.log(window.innerWidth);
+let radio = 7 ;
+if (window.innerWidth > 500 ) { radio = 4} ;
 for (let i = 0; i < btns.length; i++) {
     btns[i].onclick = function () {
         if (!states[i]) {
@@ -14,14 +17,14 @@ for (let i = 0; i < btns.length; i++) {
             answers[i].style.cssText = " display : inline-block";
             states[i] = !states[i];
             add++ ;
-            main.style.height = 100+ add*7 + "vh" ;
+            main.style.height = 100+ add*radio + "vh" ;
         }else {
             btns[i].style.cssText = "transform : rotate(0deg)";
             questions[i].classList.remove("selected");
             answers[i].style.cssText = " display : none";
             states[i] = !states[i];
             add-- ;
-            main.style.height = 100+ add*7 + "vh" ;
+            main.style.height = 100+ add*radio + "vh" ;
         }
     }
 }
